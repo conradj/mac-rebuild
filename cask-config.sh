a43f9640a7642aa82f3528b4a8af49decdabb67f
conradj <conradsjackson@gmail.com>
2017-11-11 14:28:49 +0000

    Update and rename cask-config.sh to shell-config.sh

--- cask-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-brew cask install iterm2
-brew install zsh zsh-completions
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
---
+++ shell-config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+brew install wget
+brew cask install iterm2
+brew install zsh zsh-completions
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+##### When the installation is done, edit ~/.zshrc and set ZSH_THEME="agnoster"
+##### Drop the cobalt2.zsh-theme file in to the ~/.oh-my-zsh/themes/ directory
+~/.oh-my-zsh/themes/ 
+wget https://raw.githubusercontent.com/wesbos/Cobalt2-iterm/master/cobalt2.zsh-theme
+
+https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Solarized%20Dark%20-%20Patched.itermcolors
+
+Click "view raw" to download the font.
+Open the downloaded font and press "Install Font".
+
+Set this font in iTerm2 (14px is my personal preference) (iTerm → Preferences → Profiles → Text → Change Font).
+https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf
